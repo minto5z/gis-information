1d973cf74d9b6bd27fdba70dbd46917d1790d0a2
Mahadiur Jaman Minto <minto.eatl@gmail.com>
2021-12-06 19:33:10 +0600

    codebase update

--- lib/screens/screen.dart
+++ lib/screens/screen.dart
@@ -4,7 +4,6 @@ export 'language_setting/language_setting.dart';
 export 'loading/loading.dart';
 export 'picker_screen/picker_screen.dart';
 export 'profile/account.dart';
-export 'qr_scan/qr_scan.dart';
 export 'setting/setting.dart';
 export 'signin/signin.dart';
 export 'theme_setting/theme_setting.dart';
